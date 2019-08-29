4817d2419fed8b21f7bdb55931fe2093325974eb
kavshot <cenkkavsut@hotmail.com>
2019-08-29 22:10:02 +0300

    CryptoMessagingApp

--- CryptoMessagingApp/src/app/login/login.component.ts
+++ CryptoMessagingApp/src/app/login/login.component.ts
@@ -12,37 +12,61 @@ import gql from 'graphql-tag';
 export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
-  public usernameConf: string;
+  public temp: string="";
   public passwordConf: string;  
   login: boolean = true; // switch between Login and SignUp
-  counter:number=0;
-
-addBook = gql`mutation {
+/*addBooks = gql`mutation {
   addBook(username: "", password: "", sender: "", recipient: "", currentRoom: "", passpharse: "") {
-        _id
+    _id
     username
     password
   }
-}
+}`;*/
+
+submitBook = gql`
+  mutation addBook(
+    $username: String!,
+    $password: String!,
+    $currentRoom: String!,
+    $recipient: String!,
+    $sender: String!,
+    $passpharse: String!) {
+    addBook(
+      username: $username,
+      password: $password,
+      currentRoom: $currentRoom,
+      recipient: $recipient,
+      sender: $sender,
+      passpharse: $passpharse) {
+      _id
+    }
+  }
+`;
+addBook = gql`
+  mutation addBook($username: String!,$password: String!,$currentRoom: String!,$recipient: String!,$sender: String!,$passpharse: String!) {
+  addBook(username: $username,password: $password,currentRoom: $currentRoom,recipient: $recipient,sender: $sender,passpharse: $passpharse) {
+    _id
+    }
+  }
 `;
 
 constructor(private router: Router,private route: ActivatedRoute,private apollo: Apollo) { }
 
 signIn():void{
   if(this.username!=null &&this.password!=null && this.username!=""&& this.password!="" && this.login==true){
-    //add here the query that check and for loging in p
+    //add here the query that check and for loging in
 alert('Welcome!');
 this.router.navigate(["/home"]);
 }
-else if(this.username==null&&this.password==null&&this.login==false){
+else if(this.username==null&&this.password==null&&this.password==this.passwordConf&&this.login==false){
 //this is for when a user generates an account if account is false make login true for going to the home page
-if(this.counter==1){
-this.login=true; this.counter=0;
-}else{
-this.counter=this.counter+1;
-} //here add the query for registering
-this.apollo.mutate({mutation: this.addBook}).subscribe();//this creates the account in the database
-alert('Account is generated, you will be redirected to login!');
+//this.apollo.mutate({mutation: this.addBook}).subscribe();//this creates the account in the database
+this.apollo.mutate({ mutation: this.addBook,
+variables: {
+username: this.username,password:this.password,currentRoom: this.temp,recipient: this.temp,sender: this.temp,passpharse: this.temp
+  }}).subscribe(({ data }) => {alert('got data '+data);
+  }, (error) => {alert('there was an error sending the query '+ error);});
+alert('Account is generated, you will be redirected to login!');this.login=true; 
 }
 }
 ngOnInit() {}
