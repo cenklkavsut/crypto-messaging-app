027c4d7fa7845e5dd0cb20e2daeef0ff6e263778
kavshot <cenkkavsut@hotmail.com>
2019-11-03 16:19:23 +0000

    CryptoMessaging App

--- CryptoMessagingApp/src/app/chat/chat.component.ts
+++ CryptoMessagingApp/src/app/chat/chat.component.ts
@@ -3,14 +3,10 @@ import { Router, ActivatedRoute } from "@angular/router";
 import { Apollo } from "apollo-angular";
 import gql from "graphql-tag";
 import { json } from 'body-parser';
-// const { Connection } = require("@arkecosystem/client");
-// import * as Client from "@arkecosystem/client";
-//const Client = new client("http://localhost:4003/api/v2");//the blockchain node it point to http://my.node.ip:port/api/v2
-
-// you can find the source code for these here:// https://github.com/ArkEcosystem/core/tree/master/packages/crypto/src
 import {Crypto} from "@arkecosystem/crypto"; //this allows for performing crypto operations.
+import { Connection } from "@arkecosystem/client";//this allows performing fetching operations.
 
-import { Connection } from "@arkecosystem/client";
+//const Client = new client("http://localhost:4003/api/v2");//the blockchain node it point to http://my.node.ip:port/api/v2
 
 //the application send to the recipient and take ark to sent to the user but it does not allow the library to be used
 @Component({
@@ -22,17 +18,19 @@ import { Connection } from "@arkecosystem/client";
 //it sends the message based on the passphrase and recipient and sender
 export class ChatComponent implements OnInit {
   roomName: string = "room"; //make a query that fetches the provided room
-  recipientId: string = ""; //info from wallet to send or recieve message reipient is gonne be the room name
-  SenderId: string = ""; //info from wallet to send or recieve message
   user: string = ""; //change this admin and room to a query that retrieves
   userList = new Array<string>(); //list of users
   messageText: string = ""; //the text recieved
   messageContainer: string; //temporary message container to store in array
   messageArray = new Array<string>(); //this array needs to send to the ark core blockchain but the api doesnt allow to be send over the ark
-  message = this.messageText;
+  message = this.messageText;  
+  start:boolean=false;
+  //
+  recipientId: string = ""; //info from wallet to send or recieve message reipient is gonne be the room name
+  SenderId: string = ""; //info from wallet to send or recieve message
   passPhrase: string = ""; //is needed to send messages.also it is the password you use to sign your wallet
   signed = null;
-  start:boolean=false;
+  //
   //this updates a room to the database
   updateRoom = gql`
     mutation updateRooms(
@@ -81,11 +79,9 @@ export class ChatComponent implements OnInit {
       this.start=true;
     }
     console.log(Crypto); //look at the developer console output to inspect the contents of the crypto toolset
-    //const k = crypto.Identities.Keys.fromPassphrase(this.passPhrase);//this key changes the passphrase of the wallet into a address finder
     const m = this.messageText; //the message that gets hashed to be send
     const hash = Crypto.HashAlgorithms.sha256(m); //the message gets hashed to be send to the chain
 
-    // see https://github.com/ArkEcosystem/core/blob/master/packages/crypto/src/crypto/message.ts
     const signature = Crypto.Message.sign(hash.toString(), this.passPhrase); //the signature that gets the message and sends it
     this.signed = {// this signs the message with data so it can be send over to the blockchain
       //the signed information of a string message
@@ -113,7 +109,7 @@ export class ChatComponent implements OnInit {
       if (this.messageText != "") {
         this.messageContainer = this.messageText;
         this.messageArray.push(this.messageContainer);
-        // see https://github.com/ArkEcosystem/core/blob/master/packages/crypto/src/crypto/message.ts
+
         let result = Crypto.Message.verify(this.signed.signature);//this verifies the message and allows it to be send
 
         // inspect the result of the verification process, which will be a boolean (true/falsnpm i @angular/router -se)
@@ -155,19 +151,20 @@ export class ChatComponent implements OnInit {
  async recieveMessage(){ //the client to recieve message/transaction and change resource to api if needed
   try {//here it fetches the block with data of the message 
 
-    const init = async () => {
-      const connection: Connection = new Connection("https://explorer.ark.io:8443/api");
-      let response;
-      console.log(response = await connection.api("blocks").all())
-      this.messageArray.push(response.data);
-    };
+    // const init = async () => {
+    //   const connection: Connection = new Connection("https://explorer.ark.io:8443/api");
+    //   let response;
+    //   console.log(response = await connection.api("blocks").all())
+    //   this.messageArray.push(response.data);
+    // };
     
-    init();
+    // init();
 
     // const connection = new Client.Connection("https://explorer.ark.io:8443/api");
     // const response = await connection.api('blocks').all('limit', 1);
     // return this.messageArray.push(response.data);//the block fethches it and pushed the message in the array to be displayed
-} catch (e) {
+
+  } catch (e) {
     console.log(e);
   }  
 }/**///add transaction to the api pool
